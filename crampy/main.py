a4796969a6386a80ac36cda155fafe86e574f384
Gabriel Abreu <gabrielabreu.development@gmail.com>
2024-03-09 14:56:59 -0300

    Feature/002 (#8)
    * Creating source reader
    
    * Making models more protected and updating unit tests
    
    * Creating parsing and running modules
    
    * Some fixes
    
    ---------
    
    Co-authored-by: abreu-dev <gabrieldeabreu.estudo@gmail.com>

--- crampy/main.py
+++ crampy/main.py
@@ -1,2 +1,18 @@
+from pathlib import Path
+
+from crampy.running import Transformer, Settings
+
+
+def run():
+    settings = Settings()
+    settings.convert_extension = "html"
+    settings.convert_mode = "practice_test"
+
+    source = Path("C:\\Development\\CramPy\\files\\matematica_facil.json")
+
+    Transformer(settings).transform(source)
+
+
 if __name__ == "__main__":
     print("CramPy!")
+    run()
